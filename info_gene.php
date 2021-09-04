14eda6aca388457e04ea00b5990f2a37a9cf061c
SophSM <sogusama02@gmail.com>
2021-09-04 13:58:41 -0500

    4 sep

--- info_gene.php
+++ info_gene.php
@@ -36,7 +36,7 @@ tr:not(:first-child)>* { border-left:0; }
             if ($result_gene->num_rows > 0) { ?>
             <h2><?= $gene_req; ?> GENE in Escherichia coli K-12 genome </h2> <br><br>
             <h3>Gene</h3>
-                <table border="1">
+                <table>
                   <tr>
                       <th> Gene ID </th> 
                       <th> Gene Name </th>
@@ -55,16 +55,70 @@ tr:not(:first-child)>* { border-left:0; }
                       <?php $sinonimos= $mysqli->query("SELECT object_synonym_name FROM OBJECT_SYNONYM g WHERE object_id = '" . $campos->gene_id . "'");
                       for ($num_fila = 1;  $num_fila <= $sinonimos->num_rows; $num_fila++) {
                         $campos2 = $sinonimos->fetch_object();
-                        echo $campos2->object_synonym_name. " ";
+                        echo $campos2->object_synonym_name. ", ";
                       }
                     ?>
                     </td>
                     <td><?= $campos->gene_strand;?> </td>
-                    <?php echo '<td> <a href="secuencias.php?sequence='.$campos->gene_id.'&Submit=Buscar"> '; ?> Ver secuencia completa </a> </td>
+                    <?php echo '<td> <a href="secuencias.php?sequence='.$campos->gene_id.'&type=gene"> '; ?> Ver secuencia completa </a> </td>
                   </tr>
                   <?php } ?>
                 </table>
                 <br><br>
+            <h3>Product</h3>
+              <table>
+                <tr>
+                    <th> Name </th> 
+                    <th>Synonym(s)</th>
+                    <th>Product</th>
+                    <th>Cellular Location</th>
+                    <th>Molecular weight</th>
+                    <th>Isoelectric point</th>
+                </tr>    
+                <tr>
+                  <td>
+                    <?php $product= $mysqli->query("SELECT * FROM PRODUCT g WHERE product_id IN(SELECT product_id FROM GENE_PRODUCT_LINK g WHERE gene_id= '" . $campos->gene_id. "')");
+                      for ($num_fila = 1;  $num_fila <= $product->num_rows; $num_fila++) {
+                        $campos3 = $product->fetch_object();
+                        echo $campos3->product_name;
+                      }
+                    ?>
+                  </td>
+                  <td>
+                    <?php $product_synonym= $mysqli->query("SELECT object_synonym_name FROM OBJECT_SYNONYM g WHERE object_id = '" . $campos3->product_id . "'");
+                      for ($num_fila = 1;  $num_fila <= $product_synonym->num_rows; $num_fila++) {
+                        $campos4 = $product_synonym->fetch_object();
+                        echo $campos4->object_synonym_name. ", ";
+                      }
+                    ?>
+                  </td>
+                  <?php echo '<td> <a href="secuencias.php?sequence='.$campos3->product_id.'&type=product"> '; ?> Ver secuencia de aminoacidos completa </a> </td>
+                  <td><?php echo $campos3->location ?></td>
+                  <td><?php echo $campos3->molecular_weigth?></td>
+                  <td><?php echo $campos3->isoelectric_point?></td>
+                </tr>
+              </table>
+                <br><br>
+                <h3>Operon</h3>
+              <table>
+              <tr>
+                    <th> Name </th> 
+              </tr>    
+              <tr>
+                <td>
+                <?php $operon= $mysqli->query("SELECT o.operon_name FROM OPERON o JOIN TRANSCRIPTION_UNIT t ON o.operon_id=t.operon_id JOIN TU_GENE_LINK tu ON t.transcription_unit_id=tu.transcription_unit_id JOIN GENE g ON tu.gene_id=g.gene_id AND g.gene_id = '" . $campos->gene_id. "'");
+                      for ($num_fila = 1;  $num_fila <= $operon->num_rows; $num_fila++) {
+                        $campos5 = $operon->fetch_object();
+                      }
+                      echo $campos5->operon_name;
+                    ?>
+                </td>
+              </tr>
+
+
+
+
+              </table>
             <?php
               $result_gene->close();
             }?>
--- secuencias.php
+++ secuencias.php
@@ -9,12 +9,14 @@
       error_reporting(E_ALL);
       ini_set('display_errors', '1');
       $gene_req = escapeshellcmd( $_GET["sequence"] );
+      $tipo = escapeshellcmd( $_GET["type"] );
       $mysqli = new mysqli("132.248.248.121:3306", "lcgej", "Genoma123#$", "LCGEJ");
       if ($mysqli->connect_errno) 
       {
           echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
           die();
       }
+      if ($tipo == 'gene'){
       $result_gene = $mysqli->query("SELECT * FROM GENE g WHERE gene_id = '" . $gene_req . "'");
         ?>
       <?php 
@@ -28,5 +30,22 @@
             <?php
               $result_gene->close();
             }?>
+      <?php } 
+      if ($tipo == 'product')
+      {
+   $result_product = $mysqli->query("SELECT * FROM PRODUCT g WHERE product_id = '" . $gene_req . "'");
+        ?>
+      <?php 
+            if ($result_product->num_rows > 0) { ?>
+            <h2> Sequence of aminoacids for <?= $gene_req; ?> Product </h2> <br><br>
+                <?php
+                  $campos2 = $result_product->fetch_object();
+                  echo $campos2->product_sequence;
+                ?>
+                
+            <?php
+              $result_product->close();
+            }?>
+      <?php } ?>
     </body>
 </html>
